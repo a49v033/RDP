2d60bb43207c51de56ad97e786e19bc49f110158
Tiramitzu <tiramitzu.c@gmail.com>
2021-02-03 20:53:08 +0700

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing the server, please wait..." >&2
 {
-sudo useradd -m ubuntu
-sudo adduser ubuntu sudo
-echo 'ubuntu:12345678' | sudo chpasswd
+sudo useradd -m Tira
+sudo adduser Tira sudo
+echo 'Tira:TiaraR01?' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,13 +18,13 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser ubuntu chrome-remote-desktop
+sudo adduser tira chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup has completed " >&2 ||
 printf "\nAn error has occured " >&2
 printf '\nPlease check https://remotedesktop.google.com/headless And copy the command line of Debian Linux and then paste it\n'
 read -p "Command: " CRP
-su - ubuntu -c """$CRP"""
+su - tira -c """$CRP"""
 printf 'Please check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
